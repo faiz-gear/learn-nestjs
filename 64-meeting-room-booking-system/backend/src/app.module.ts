a5eac07e3e65481b4327aa107492151068397f88
faiz-gear <1824056030@qq.com>
2024-04-09 22:33:14 +0800

    meeting-room-booking-system-23

--- 64-meeting-room-booking-system/backend/src/app.module.ts
+++ 64-meeting-room-booking-system/backend/src/app.module.ts
@@ -14,6 +14,8 @@ import { JwtModule } from '@nestjs/jwt';
 import { APP_GUARD } from '@nestjs/core';
 import { LoginGuard } from './login.guard';
 import { PermissionGuard } from './permission.guard';
+import { MeetingRoomModule } from './meeting-room/meeting-room.module';
+import { MeetingRoom } from './meeting-room/entities/meeting-room.entity';
 
 @Module({
   imports: [
@@ -34,7 +36,7 @@ import { PermissionGuard } from './permission.guard';
           database,
           synchronize: true,
           logging: true,
-          entities: [User, Role, Permission],
+          entities: [User, Role, Permission, MeetingRoom],
           poolSize: 10,
           connectorPackage: 'mysql2',
         };
@@ -61,6 +63,7 @@ import { PermissionGuard } from './permission.guard';
     UserModule,
     RedisModule,
     EmailModule,
+    MeetingRoomModule,
   ],
   controllers: [AppController],
   providers: [
