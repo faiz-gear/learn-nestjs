02b80ceb7a683b10003181f8e4d8bb34ade2643f
faiz-gear <1824056030@qq.com>
2024-04-28 23:40:51 +0800

    restful api style

--- 81-nest-mongoose/src/user/user.controller.ts
+++ 81-nest-mongoose/src/user/user.controller.ts
@@ -3,9 +3,9 @@ import {
   Get,
   Post,
   Body,
-  Patch,
   Param,
   Delete,
+  Put,
 } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
@@ -15,12 +15,12 @@ import { UpdateUserDto } from './dto/update-user.dto';
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @Post('create')
+  @Post()
   create(@Body() createUserDto: CreateUserDto) {
     return this.userService.create(createUserDto);
   }
 
-  @Get('list')
+  @Get('users')
   findAll() {
     return this.userService.findAll();
   }
@@ -30,7 +30,7 @@ export class UserController {
     return this.userService.findOne(id);
   }
 
-  @Patch(':id')
+  @Put(':id')
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.userService.update(id, updateUserDto);
   }
