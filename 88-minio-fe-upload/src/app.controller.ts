aed177f186c2a6e8d6ee01b82f3eb9bfe66f41fe
faiz-gear <1824056030@qq.com>
2024-05-06 14:00:07 +0800

    minio fe upload

---
+++ 88-minio-fe-upload/src/app.controller.ts
@@ -0,0 +1,37 @@
+import { Controller, Get, Inject, Query } from '@nestjs/common';
+import { AppService } from './app.service';
+import { MINIO_CLIENT } from './minio/minio.module';
+import * as Minio from 'minio';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Inject(MINIO_CLIENT)
+  private minioClient: Minio.Client;
+
+  @Get('test')
+  async test() {
+    try {
+      await this.minioClient.fPutObject(
+        'aaa',
+        'package.json',
+        './package.json',
+      );
+      return 'http://localhost:9000/aaa/package.json';
+    } catch (e) {
+      console.log(e);
+      return '上传失败';
+    }
+  }
+
+  @Get('presignedUrl')
+  async presignedUrl(@Query('name') name: string) {
+    return this.minioClient.presignedPutObject('aaa', name);
+  }
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+}
