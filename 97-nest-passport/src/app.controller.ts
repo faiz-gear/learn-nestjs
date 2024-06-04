3a399137b7d7d551914323d6759cd0ee8a508bee
faiz-gear <1824056030@qq.com>
2024-06-04 23:22:20 +0800

    nest-passport-2

--- 97-nest-passport/src/app.controller.ts
+++ 97-nest-passport/src/app.controller.ts
@@ -1,9 +1,36 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
+import { LocalAuthGuard } from './auth/local-auth.guard';
+import { AuthService } from './auth/auth.service';
+import { JwtAuthGuard } from './auth/jwt-auth.guard';
+import { Public } from './auth/is-public.decorator';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly authService: AuthService,
+  ) {}
+
+  @Public()
+  @UseGuards(LocalAuthGuard)
+  @Post('auth/login')
+  async login(@Request() req) {
+    // return req.user;
+    return this.authService.login(req.user);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  getProfile(@Request() req) {
+    return req.user;
+  }
+
+  @Public()
+  @Get('/list')
+  findAll() {
+    return [];
+  }
 
   @Get()
   getHello(): string {
